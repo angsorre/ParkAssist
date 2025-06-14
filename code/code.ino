991217de1262f50b71dca0fea696301c9ada3543
angsorre <angelo.sorrentino@studenti.unimi.it>
2025-06-14 16:11:03 +0200

    TOTEST: calibration

--- code/code.ino
+++ code/code.ino
@@ -1,9 +1,15 @@
 // every distance measure is in cm;
 #include <math.h>
-float MOUNT_DISTANCE = 40.0; // distance to the wall at which the laser is mounted
+#include <EEPROM.h>
+
+
+float MOUNT_DISTANCE = 40.0; // distance to the wall (cm) at which the laser is mounted
+float addr_MOUNT_DISTANCE = 1;
 #define LIMIT_ANGLE 60 // angle at which the servo should be limited
 #define BUTTON_PIN 8  // button that triggers the distance update
 #define POTENTIOMETER_PIN 14 // pin to which the distance potentiometer is connected
+int ZERO_DEG = 0;
+int addr_ZERO_DEG = 0;
 
 /// ------- TASKS -------- //
 #include <TaskScheduler.h>
@@ -40,11 +46,28 @@ Task tUpdateMountDistance(TASK_SECOND,
                   &ts,
                   true);
 
+// -- ZERO_DEG calibration
+void updateZeroDeg();
+Task tupdateZeroDeg(TASK_SECOND * 5,
+                  TASK_FOREVER,
+                  &updateZeroDeg,
+                  &ts,
+                  true);
+
+
+// ------ SETUP & LOOP --------- //
 void setup() {
   // put your setup code here, to run once:
   //Serial.begin(115200);
   pointer.attach(9);
   pinMode(BUTTON_PIN, INPUT);
+  ZERO_DEG = EEPROM.read(addr_ZERO_DEG);
+  // EEPROM is read in bytes, however using an int is safe as value don't go over 255
+  float temp;
+  EEPROM.get(addr_MOUNT_DISTANCE, temp);
+  if (temp < 30.0) {
+    MOUNT_DISTANCE = temp;
+  }
 }
 
 void loop() {
@@ -59,14 +82,26 @@ void readMeasure() {
 
 void moveLaser() {
   int deg = atan(distance/MOUNT_DISTANCE) * 180/M_PI;
-  if(deg < LIMIT_ANGLE) {
-    pointer.write(deg);
-  }
+  // received degree is mapped to match the zero of calibration
+  deg = map(constrain(deg, ZERO_DEG, LIMIT_ANGLE), ZERO_DEG, LIMIT_ANGLE, 0, LIMIT_ANGLE);
+  pointer.write(deg);
 }
 
 void updateMountDistance() {
   if (digitalRead(BUTTON_PIN) == HIGH) {
     MOUNT_DISTANCE = map(analogRead(POTENTIOMETER_PIN), 0,1023, 30, 101);
+    EEPROM.put(addr_MOUNT_DISTANCE, MOUNT_DISTANCE);
   }
 }
 
+void updateZeroDeg() {
+  int deg = atan(distance/MOUNT_DISTANCE) * 180/M_PI;
+  if (deg < LIMIT_ANGLE) {
+    ZERO_DEG = deg;
+    EEPROM.update(addr_ZERO_DEG, deg); 
+  }
+  /*
+    EEPROM writes are limited to 100k cycles, write() method should be as valuable as the value isn't supposed to change so often
+    but update can save write cycles if it doesn't change.
+  */
+}
